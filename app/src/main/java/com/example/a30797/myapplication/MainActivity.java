06beb98639e3cedafe788ce5f4178609f96a9e5a
LonglyWolf <814484626@qq.com>
2018-12-12 14:10:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a30797/myapplication/MainActivity.java
@@ -0,0 +1,186 @@
+package com.example.a30797.myapplication;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.a30797.myapplication.compute.Calculate;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    String edit = new String();//用于存储edit中的数
+    int bracket_flag01 = 0 ;//用于判断括号合法性
+    int flag01  = 0 ;//用于匹配运算模式
+    int flag02 = 0 ;//用于查看是否反复点击同一模式按钮
+    int flag03 = 0 ;//用于查看 前一轮是否为转换
+    TextView tv_save_up;//记录框
+    TextView tv_save_dowm;
+    TextView tv;//输出框
+    Button gri_bn[] = new Button[20];//网格布局 按钮
+    GridLayout gridLayout;//网格布局
+    String[] chars = new String[]{//定义4*4 16个按钮文本数组
+            "C","←","%","+",
+            "7","8","9","--",
+            "4","5","6","*",
+            "1","2","3","/",
+            "( )","0",".","=",
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //设置输出框
+        tv = (TextView) findViewById(R.id.textview);
+        //设置输出框
+        tv_save_up = (TextView) findViewById(R.id.textview_up);
+        //设置输出框
+        tv_save_dowm = (TextView) findViewById(R.id.textview_down);
+        gridLayout = (GridLayout) findViewById(R.id.root);
+        iniClick();
+    }
+    /*
+    点击事件
+     */
+    private void iniClick(){
+        for( int i = 0 ; i < chars.length ; i++){
+            //chat用于传递i到监听事件
+            final int chat = i;
+            gri_bn[i] = new Button(this);
+            gri_bn[i].setText(chars[i]);//设置按钮的字号大小
+            gri_bn[i].setTextSize(40);//设置数字按钮颜色
+            gri_bn[i].setTextColor(Color.WHITE);//按钮四周空白区域
+            gri_bn[i].setPadding(43 , 43 , 43 , 43);//设置按钮背景
+            gri_bn[i].setBackgroundResource(R.drawable.shape_button_01);//指定按钮组所在行
+            GridLayout.Spec rowSpec = GridLayout.spec(i / 4 + 2);
+            //指定数组所在列
+            GridLayout.Spec columeSpec = GridLayout.spec(i % 4);
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams(
+                    rowSpec , columeSpec);
+            gridLayout.addView(gri_bn[i] , params);
+            //设置监听事件
+            gri_bn[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if( chars[chat] .equals("=") ){
+                        String presentString = tv.getText().toString();
+                        char endChar[] = presentString.substring(presentString.length()-1,presentString.length()).toCharArray();
+                        if ( tv.getText().toString().length() == 0 ){
+                            Toast.makeText(getApplicationContext(),"请输入计算式",Toast.LENGTH_SHORT).show();
+                        }else if ( bracket_flag01 != 0 ){
+                            Toast.makeText(getApplicationContext(),"请正确输入",Toast.LENGTH_SHORT).show();
+                        }else if ( '0' > endChar[0] || '9' < endChar[0] ){
+                            Toast.makeText(getApplicationContext(),"请不要以运算符结尾",Toast.LENGTH_SHORT).show();
+                        }else if ( flag02 != 1 ){//防止反复点击
+                            edit = tv.getText().toString();//保存文本框中内容 用于传递
+                            StringBuffer stringBuffer = new StringBuffer();
+                            //拼接字符串形成 # 结尾
+                            stringBuffer.append(edit);
+                            stringBuffer.append('#');
+                            edit = stringBuffer.toString();
+                            //选择操作类型
+                            flag01 = 1;
+                            //设置 上方存储显示栏 文字
+                            tv_save_up.setText(tv_save_dowm.getText().toString());
+                            tv_save_dowm.setText(tv.getText().toString());
+                            //开始配对并运算
+                            ChooseFlag(tv);
+                            tv.requestFocus();//焦点回到文本框
+                            flag02 = 1 ;
+                            bracket_flag01 = 0 ;
+                        }else {
+                            Toast.makeText(getApplicationContext(),"请勿仿佛点击",Toast.LENGTH_SHORT).show();
+                        }
+
+                    }else if( chars[chat] == "C" ){
+                        flag02 = 0 ;
+                        tv.setText("");
+                        bracket_flag01 = 0 ;
+                    }else if( chars[chat] == "←" ){
+                        if ( tv.getText().toString().length() > 0 ){//删除前判断“（ ）”
+                            char array[] = tv.getText().toString().toCharArray();
+                            if (array[array.length - 1 ] == '('){
+                                bracket_flag01 -- ;
+                            }else if ( array[array.length - 1 ] == ')' ){
+                                bracket_flag01 ++ ;
+                            }
+                            tv.setText(tv.getText().toString().substring(0,
+                                    tv.getText().toString().length() - 1));
+                            flag02 = 0 ;
+                        }
+                    }else if( chars[chat] == "( )" ){
+                        if ( tv.getText().toString().length() > 0 ){
+                            char array[] = tv.getText().toString().toCharArray();
+                            Log.e("789",String.valueOf(array[ array.length - 1 ]));
+                            if ( ( array[array.length - 1 ] > '9' ||  array[ array.length - 1 ] < '0' )
+                                    && array[  array.length - 1 ] != ')' ){//判断前一个字符类型
+                                tv.append( "(" );
+                                bracket_flag01 ++ ;
+                                flag02 = 0 ;
+                            }else if ( bracket_flag01 != 0 ) {
+                                tv.append( ")" );
+                                bracket_flag01 -- ;
+                                flag02 = 0 ;
+                            }
+                        }else{
+                            tv.append( "(" );
+                            bracket_flag01 ++ ;
+                            flag02 = 0 ;
+                        }
+                    }else if ( (chars[chat]).toCharArray()[0] <= '9' &&  (chars[chat]).toCharArray()[0] >= '0' ) {
+                        tv.append(chars[chat]);
+                        flag02 = 0 ;
+                    }else if ( tv.getText().toString().length() > 0 ){
+                        char array[] = tv.getText().toString().toCharArray();
+                        if ( ( array[ array.length - 1 ] <= '9' &&  array[ array.length - 1 ] >= '0' )
+                                || array[ array.length - 1 ] == ')' ) {//判断前一个字符类型
+                            if( chars[chat] == "--" ){
+                                tv.append("-");
+                                flag02 = 0 ;
+                            }else{
+                                tv.append( chars[chat] );
+                                flag02 = 0 ;
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        tv_save_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(tv_save_up.getText());
+                tv.requestFocus();//焦点回到文本框
+            }
+        });
+        tv_save_dowm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(tv_save_dowm.getText());
+                tv.requestFocus();//焦点回到文本框
+            }
+        });
+    }
+
+    /*
+     * 判断当前操作
+     */
+    public void ChooseFlag(TextView jt){
+        Calculate text = new Calculate();
+        text.toStringArray(tv.getText().toString());
+        text.outPutCalculate();
+        try {
+            text.toPostfix();
+            jt.setText(text.postfix());
+        }catch (Exception e){
+            Toast.makeText(MainActivity.this,"请正确输入",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/a30797/myapplication/compute/Calculate.java
@@ -0,0 +1,283 @@
+package com.example.a30797.myapplication.compute;
+
+import java.math.BigDecimal;
+import java.util.Stack;
+
+public class Calculate {
+
+    private Stack<String> num; //后缀用栈 中转后数字栈
+
+    private Stack<String> charnum;//中转后字符栈
+
+    private String []calculate;//存字符串数组
+
+    private int calculateLength;//字符串数组长度
+
+    public Calculate() {
+        // TODO Auto-generated constructor stub
+        num = new Stack<>(); //后缀用栈 中转后数字栈
+
+        charnum = new Stack<>();//中转后字符栈
+
+        calculate = new String[1000];//存字符串数组
+
+        calculateLength = 0 ;//字符串数组长度
+    }
+
+    //转字符串数组
+    public void toStringArray(String input) {
+        boolean pointFalg = false;
+        char charArray[] = input.toCharArray();
+        double number = 0;//用于导入多位数
+        int j = 0 ;//用于计入当前字符串数组的位数
+        int sizeOfArray = charArray.length;
+        int pointBelow =1
+                ;
+        for(int i = 0 ; i < sizeOfArray ; i++){
+            if(charArray[i] == '('){
+                calculate[j++] = "(";
+
+            }else if(charArray[i] == ')'){
+                calculate[j++] = ")";
+
+            }else if (charArray[i] == '+') {
+                calculate[j++] = "+";
+
+            }else if (charArray[i] == '-') {
+                calculate[j++] = "-";
+
+            }else if (charArray[i] == '*') {
+                calculate[j++] = "*";
+
+            }else if (charArray[i] == '/') {
+                calculate[j++] = "/";
+
+            }else if (charArray[i] == '%') {
+                calculate[j++] = "%";
+
+            }else if (charArray[i] == '#') {
+                calculate[j++] = "#";
+
+            }else if (charArray[i] == '.') {
+                System.out.println("find new . :");
+                pointBelow = 1;
+//				sizeOfArray -- ;
+                pointFalg = true;
+            }else {
+                String str=String.valueOf(charArray[i]);
+                if (pointFalg == false) {
+                    System.out.println("1:" + number);
+                    number = number * 10 + Double.parseDouble(str);
+                }else {
+                    System.out.println("2:" + charArray[i]);
+                    number = number + Double.parseDouble(str) * Math.pow(0.1, pointBelow);
+                }
+                System.out.println("3:" + number + "i==" + i);
+                if( (i + 1) == sizeOfArray ||( charArray[i+1] < '0' || charArray[i+1] > '9' ) && charArray[i+1] != '.'){
+
+                    if ( (i + 1) != sizeOfArray && charArray[i+1] == ' ') {
+                        i++;
+                    }
+                    calculate[j++] = String.valueOf(number);
+                    System.out.println("number:" + number);
+                    number = 0 ;
+                    pointFalg = false;
+                }
+            }
+            System.out.println("---z->" + calculate[i]);
+        }
+        calculateLength = j-- ;//不--会将‘#’存入
+    }
+
+    public void outPutCalculate() {
+        for(int i = 0 ;  i < calculateLength ; i ++ ){
+            System.out.println(calculate[i]);
+        }
+    }
+
+    public void CalculateToZero() {
+        for(int i = 0 ;  i < calculateLength ; i ++ ){
+            calculate[i]= calculate[999] ;
+        }
+    }
+
+    //中缀转后缀
+    public void toPostfix() {
+        // TODO Auto-generated method stub
+        System.out.println("789");
+        int sum = 0 ;//用于记入”（）“总个数
+        int j = 0 ;//用于读到”）“时循环出栈
+        String outStack = null;
+        charnum.push(null);
+        System.out.println(calculateLength);
+        for( int i = 0 ; i < calculateLength ; i ++){
+            System.out.println(calculate[i]);//-----------------------------
+            if ( calculate[i].equals("(")) {
+                charnum.push(calculate[i]);
+                System.out.println("1-1  charpush  " + calculate[i]);//-----------------------------
+                sum ++ ;
+            }else if ( calculate[i].equals(")") ) {
+                System.out.println("2-1  charpush  " + calculate[i]);//-----------------------------
+                outStack = charnum.pop();//进入前先出一个
+                System.out.println("2-1  charpush  " + outStack);//-----------------------------
+                while ( !outStack.equals("(") ){
+                    System.out.println("2-2  charpush  " + outStack);//-----------------------------
+                    num.push(outStack);
+                    outStack = charnum.pop();
+                }//最后一次outStack正好接到”（“不入栈
+                System.out.println("qiangxing 1 = " + outStack );
+//					outStack = charnum.pop();
+                System.out.println("qiangxing 2 = " + outStack );
+                sum ++ ;
+            }else if (calculate[i].equals("*")) {
+                outStack = charnum.pop();
+                charnum.push(outStack);
+                System.out.println("3-2  charpush  " + outStack);//-----------------------------
+                while( ( outStack == "*" || outStack == "/" || outStack == "%" ) && !(outStack == null) ){
+                    System.out.println("3-1  charpush  " + outStack);//-----------------------------
+                    num.push(outStack);
+                    charnum.pop();//由于前第三行又将outStack存入栈中，座椅此处再次弹出
+                    outStack = charnum.pop();
+                    charnum.push(outStack);
+
+                }
+                System.out.println("3-3  charpush  " + outStack);//-----------------------------
+                charnum.push("*");
+            }else if (calculate[i].equals("%")) {
+                outStack = charnum.pop();
+                charnum.push(outStack);
+                System.out.println("3-2  charpush  " + outStack);//-----------------------------
+                while( ( outStack == "*" || outStack == "/" || outStack == "%" ) && !(outStack == null) ){
+                    System.out.println("3-1  charpush  " + outStack);//-----------------------------
+                    num.push(outStack);
+                    charnum.pop();//由于前第三行又将outStack存入栈中，座椅此处再次弹出
+                    outStack = charnum.pop();
+                    charnum.push(outStack);
+
+                }
+                System.out.println("3-3  charpush  " + outStack);//-----------------------------
+                charnum.push("%");
+            }else if (calculate[i].equals("/")) {
+                System.out.println("5-1-0  charpush  " + "1-1-1-1-1-1-1-1");//-----------------------------
+                outStack = charnum.pop();
+                System.out.println("5-1-1  charpush  " + "2-2-2-2-2-2-22-2");//-----------------------------
+                charnum.push(outStack);
+                System.out.println("4-1  charpush  " + outStack);//-----------------------------
+                while( ( outStack == "*" || outStack == "/" || outStack == "%") && !(outStack == null) ){
+                    System.out.println("4-2  charpush  " + outStack);//-----------------------------
+                    num.push(outStack);
+                    charnum.pop();//由于前第三行又将outStack存入栈中，座椅此处再次弹出
+                    outStack = charnum.pop();
+                    charnum.push(outStack);
+                }
+                System.out.println("4-3  charpush  " + outStack);//-----------------------------
+                System.out.println("5-1-2  charpush  " + outStack);//-----------------------------
+                charnum.push("/");
+            }else if (calculate[i].equals("+")) {
+                outStack = charnum.pop();
+                charnum.push(outStack);
+                System.out.println("5-1  charpush  " + outStack);//-----------------------------
+                while( !(outStack=="(") && !(outStack == null) ){
+                    System.out.println("5-2  charpush  " + outStack);//-----------------------------
+                    num.push(outStack);
+                    charnum.pop();
+                    outStack = charnum.pop();
+                    charnum.push(outStack);
+                }
+                System.out.println("5-3  charpush  " + outStack);//-----------------------------
+                charnum.push("+");
+            }else if (calculate[i].equals("-")) {
+                outStack = charnum.pop();
+                charnum.push(outStack);
+                System.out.println("6-1  charpush  " + outStack);//-----------------------------
+                while(  !(outStack=="(") && !(outStack == null)  ){
+                    System.out.println("6-2  charpush  " + outStack);//-----------------------------
+                    num.push(outStack);
+                    charnum.pop();
+                    outStack = charnum.pop();
+                    charnum.push(outStack);
+                }
+                System.out.println("6-3  charpush  " + outStack);//-----------------------------
+                charnum.push("-");
+            }else {
+                System.out.println("7-7    " + calculate[i]);
+                num.push(calculate[i]);
+            }
+        }
+        System.out.println("匹配结束" + outStack);
+        outStack = charnum.pop();
+        System.out.println("finish 1 == " + outStack);
+        while ( outStack != null ) {
+            num.push(outStack);
+            outStack = charnum.pop();
+            System.out.println("finish 2 == " + outStack);
+        }
+        calculateLength  = calculateLength - sum ;
+        System.out.println( "0.0.0.0  charpush  " );//-----------------------------
+        System.out.println("sum = " + sum + " calculate = " +
+                calculateLength + "calculateLength-sum = " + (calculateLength-sum));
+        System.out.println("over ~~~~~0 ");
+        Stack<String> zanshi = new Stack<>();
+//			num.pop();
+        for(int i = 0 ; i < calculateLength ; i ++ ){
+            zanshi.push(num.pop());
+//				System.out.println(num.pop());
+        }
+        CalculateToZero();
+        System.out.println("over ~~~~~1 ");
+        for(int i = 0 ; i < calculateLength ;i ++ ){
+            calculate[i] = zanshi.pop();
+        }
+        System.out.println("over ~~~~~2 ");
+        for(int i = 0 ; i < calculateLength ;i ++ ){
+            System.out.println(calculate[i]);
+        }
+        System.out.println("over ~~~~~3 ");
+//			num.push("#");
+    }
+
+    //后缀计算
+    public String postfix() {
+        BigDecimal a  , b ;//栈中弹出的两数
+        BigDecimal sum ;//求两数运算
+        for (int i = 0; i < calculateLength ; i++ ) {
+//				System.out.println("目前符号：" + calculate[i]);
+            if (i == 0) {
+                num.push(calculate[i]);
+            }else if (calculate[i].equals("+") ) {
+                b = new BigDecimal(num.pop());
+                a = new BigDecimal(num.pop());
+                sum = a.add(b) ;
+                num.push(String.valueOf(sum));
+
+            }else if (calculate[i].equals("-") ) {
+                b = new BigDecimal(num.pop());
+                a = new BigDecimal(num.pop());
+                sum = a.subtract(b) ;
+                num.push(String.valueOf(sum));
+
+            }else if (calculate[i].equals("*") ) {
+                b = new BigDecimal(num.pop());
+                a = new BigDecimal(num.pop());
+                sum = a.multiply(b) ;
+                num.push(String.valueOf(sum));
+
+            }else if (calculate[i].equals("/") ) {
+                b = new BigDecimal(num.pop());
+                a = new BigDecimal(num.pop());
+                sum = a.divide(b) ;
+                num.push(String.valueOf(sum));
+
+            }else if (calculate[i].equals("%") ) {
+                b = new BigDecimal(num.pop());
+                a = new BigDecimal(num.pop());
+                sum = a.divideAndRemainder(b)[1] ;
+                num.push(String.valueOf(sum));
+            }else {
+                num.push(calculate[i]);
+            }
+        }
+        return num.pop();
+    }
+
+}
